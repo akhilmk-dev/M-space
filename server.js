a23f70236356dbd172ed6abc63af1477829eaedd
Akhil M K <akhilmk@intertoons.com>
2025-10-07 12:09:32 +0530

    cors error

--- server.js
+++ server.js
@@ -29,11 +29,8 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors({origin: ["http://localhost:3000"], 
+    credentials: true, }));
 // Increase limit to 50mb or more, as needed
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
